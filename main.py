c1e541843f54580736805b54c324d8963226366e
majidahmadics <118940088+majidahmadics@users.noreply.github.com>
2024-10-15 19:08:40 +0200

    finish data ingestion pipeline stage

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from e2e_red_wine import logger
+from src.e2e_red_wine import logger
+from src.e2e_red_wine.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info("This is an easy import logger!")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
