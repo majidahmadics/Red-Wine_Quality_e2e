ca60af3b5dad15f58f9b7b6704eb00f4e8b7b870
majidahmadics <118940088+majidahmadics@users.noreply.github.com>
2024-10-15 18:59:53 +0200

    implimenting the data ingestion pipeline stage

--- src/e2e_red_wine/utils/common.py
+++ src/e2e_red_wine/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from mlProject import logger
+from e2e_red_wine import logger
 import json
 import joblib
 from ensure import ensure_annotations
